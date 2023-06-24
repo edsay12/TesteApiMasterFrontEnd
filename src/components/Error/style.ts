f9ea998d79dca3141e2bde82ac0587c909e3296a
edsay12 <edvandearaujo2@hotmail.com>
2023-06-24 18:30:16 -0300

    feat: ajustes na responsividade

--- src/components/Error/style.ts
+++ src/components/Error/style.ts
@@ -3,9 +3,15 @@ import styled from "styled-components";
 
 
 export const ErrorMessage = styled.h1`
-    
+    width: 100%;
+    margin-top: 20px;
     color: black;
     text-transform: uppercase;
+    text-justify: auto;
+    justify-content: center;
+    max-width: 400px;
+    text-align: center;
+    
     
     span{
         color: ${({theme})=> theme.default.colors.purple};
@@ -14,7 +20,9 @@ export const ErrorMessage = styled.h1`
 export const ErrorContainer = styled.div`
     width: 100%;
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
+    gap: 20px;
     justify-content: center;
 
 `
